ee4dd4392e20c1c08b7559ed5b37644c11c91e23
Said Robley <saidrobley@gmail.com>
2018-05-18 15:34:16 -0600

    add insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,3 +1,20 @@
+import java.util.Arrays;
+
 public class InsertionSort {
+	
+	public static void main(String[] args) {
+		insertionSort(new int[]{2, 9, 4, 8, 1, 6});
+	}
+	
+	public static int[] insertionSort(int[] arr) {
+		for(int i = 1; i < arr.length; i++) {
+			int temp = arr[i];
+			for(int j = i - 1; j >= 0 && temp < arr[j]; j--) {
+				arr[j + 1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		return arr;
+	}
 	
 }
