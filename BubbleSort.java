e494154a09dfd393e41bcd4adeb7d3e51625596b
Said Robley <saidrobley@gmail.com>
2018-05-22 17:56:47 -0600

    improve bubble sort for sorted arrays

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,12 +8,17 @@ public class BubbleSort {
 	}
 	
 	public static int[] bubbleSort(int[] arr ) {
-		for(int i = 1; i < arr.length; i++) {
+		boolean needNextPass = true;
+		
+		for(int i = 1; i < arr.length && needNextPass; i++) {
+			needNextPass = false;
 			for(int j = 0; j < arr.length - i; j++) {
 				if(arr[j] > arr[j + 1]) {
 					int temp = arr[j];
 					arr[j] = arr[j+ 1];
 					arr[j + 1] = temp;
+					
+					needNextPass = true;
 				}
 			}
 		}
